bc8fe0743002ece4800a1a6134462b4a89a7e81d
sneh1811 <41875298+sneh1811@users.noreply.github.com>
2025-02-28 17:16:33 +0530

    Update NasaAPOD_UI.py

--- NasaAPOD_UI.py
+++ NasaAPOD_UI.py
@@ -14,7 +14,7 @@ st.markdown("""
     }
     .title {
         text-align: center;
-        font-size: 3em;
+        font-size: 2.5em;
         letter-spacing: 2px;
         color: #f39c12;
         text-shadow: 0 0 15px rgba(243, 156, 18, 0.8);
